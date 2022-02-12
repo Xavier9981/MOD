f25b11d4aea7dcf212de5cd5d7d21914587bb31d
Xavier K Muneku <xavier.muneku@gmail.com>
2022-02-12 02:43:11 +0100

    modified the file

--- src/script/slides-show-script.js
+++ src/script/slides-show-script.js
@@ -1,6 +1,39 @@
+var slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+  setTimeout(showSlides, 15000); // Change image every 15 seconds
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+  setTimeout(showSlides, 15000); // Change image every 15 seconds
+}
+
+/**/ function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  setTimeout(showSlides, 15000); //Change image every 15 seconds
+}
 
 //main slide show
-var slideIndex = 0;
+v; /*ar slideIndex = 0;
 showSlides();
 
 function showSlides() {
@@ -8,17 +41,19 @@ function showSlides() {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
   setTimeout(showSlides, 15000); // Change image every 15 seconds
-}  
+}*/
 
 //min slide show
 var minSlideIndex = 0;
@@ -29,14 +64,16 @@ function minShowSlides() {
   var minSlides = document.getElementsByClassName("myMinSlides");
   var dots = document.getElementsByClassName("minDot");
   for (i = 0; i < minSlides.length; i++) {
-    minSlides[i].style.display = "none";  
+    minSlides[i].style.display = "none";
   }
   minSlideIndex++;
-  if (minSlideIndex > minSlides.length) {minSlideIndex = 1}    
+  if (minSlideIndex > minSlides.length) {
+    minSlideIndex = 1;
+  }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  minSlides[minSlideIndex-1].style.display = "block";  
-  dots[minSlideIndex-1].className += " active";
+  minSlides[minSlideIndex - 1].style.display = "block";
+  dots[minSlideIndex - 1].className += " active";
   setTimeout(minShowSlides, 5000); // Change image every 5 seconds
 }
